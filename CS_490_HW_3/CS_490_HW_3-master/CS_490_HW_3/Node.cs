1f2ea56bcc40fb252d2a786a2c7d802c67545346
DESKTOP-OH5MN3G\Seth <sic0001@uah.edu>
2019-04-09 18:32:43 -0500

    urua[fh

---
+++ CS_490_HW_3/CS_490_HW_3-master/CS_490_HW_3/Node.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace CS_490_HW_3
+{
+    public class Node
+    {
+        Random myRand = new Random();
+        public int processID;
+        public int Priority;
+        public int timeSlice;
+       
+        public Node()
+        {
+            processID = myRand.Next(50000);
+            Priority = myRand.Next(21);
+            timeSlice = myRand.Next(3000);
+            Thread.Sleep(300);
+        }
+
+        public void Execute()
+        {
+            Thread.Sleep(timeSlice);
+            Console.WriteLine("Process ID: " + this.processID + " Priority: "
+                + this.Priority + " Time: " + DateTime.Now);
+        }
+    }
+}
---
+++ CS_490_HW_3/heapWatcher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CS_490_HW_3
+{
+    class heapWatcher
+    {
+        MinHeap myHeap;
+        int idle;
+        public heapWatcher(int idle, MinHeap myHeap)
+        {
+            this.myHeap = myHeap;
+            this.idle = idle;
+        }
+        public void run()
+        {
+            Console.WriteLine("Who watches the Heapwatcher...,? who knows but heapWatcher started so bugger off you gonna cry? piss your pants maybe? maybe shit and cum??");
+            while (myHeap.getSize() > 0)
+            {
+                try
+                {
+                    Thread.Sleep(1000);
+                }
+                catch (ThreadInterruptedException ex)
+                {
+                }
+            }
+            Console.WriteLine("HeapWatcher is exciting...!(?)");
+        }
+    }
+}
